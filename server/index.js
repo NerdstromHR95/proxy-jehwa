727f8ca2067d13e6d2d15296cce1c88f0bb5ddd6
Jehwa Shin <s.jehwa@gmail.com>
2018-06-12 20:14:13 -0700

    connect port# 3002

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express ();
-
-// app.use(express.json());
+// const request = require('request');
+const proxy = require('http-proxy-middleware');
 
 app.use('/:itemNo', express.static(path.join(__dirname, '/../public')));
+
+app.use('/:itemNo/reviews/*', proxy({target: 'http://localhost:3002'}))
+
 
 app.listen(3000, () => console.log('proxy server is listening on port 3000'));
